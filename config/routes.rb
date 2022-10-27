530dd3cda5dd457181fb9f4042d7b4b98634217b
christelle bettoni <christelle.bettoni@gmail.com>
2022-10-27 14:13:36 +0200

    modif dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#index'
+  root 'dashboard#index'
 
-  get "dashboard", to: "dashboard#index"
+
 
   resources :boards do
     resources :lists, except: :show
